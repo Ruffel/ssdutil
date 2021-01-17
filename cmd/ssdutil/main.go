d01f7272d592769d36cadcb37a9018008c62afe3
Steven Cartmell <45599587+Ruffel@users.noreply.github.com>
2021-01-17 17:30:59 +0000

    feat(cli): Print the build version on application start up

--- cmd/ssdutil/main.go
+++ cmd/ssdutil/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"os"
 
+	"github.com/Ruffel/ssdutil/internal/build"
 	"github.com/Ruffel/ssdutil/pkg/cmd/root"
 )
 
 func main() {
+	println("ssdutil " + build.Version)
+
 	cmd := root.NewCmdRoot()
 
 	if _, err := cmd.ExecuteC(); err != nil {
